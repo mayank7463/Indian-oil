b54879cbffc923f8e0f9991c77342c84b285afb2
mayank7463 <mayank18091999@gmail.com>
2024-07-31 10:46:39 +0530

    changes done

--- src/Pages/Landingpage/LeaderShipteam.jsx
+++ src/Pages/Landingpage/LeaderShipteam.jsx
@@ -10,7 +10,7 @@ const LeaderShipTeam = () => {
       <motion.img initial={{y:"30px",opacity:0}} whileInView={{y:0,opacity:1}} transition={{duration:0.7,ease:"easeInOut"}}  src="./Energised Leadership Team.svg" alt="" />
       <div className=''>
      <motion.img initial={{x:"-200px",opacity:0}} whileInView={{x:0,opacity:1}} transition={{duration:0.7,ease:"easeInOut"}}  className='chairman' src="./Energised/el-1.jpg" alt="" />
-     <motion.img initial={{x:"200px",opacity:0}} whileInView={{x:0,opacity:1}} transition={{duration:0.7,ease:"easeInOut"}}  className='absolute bottom-[0%] w-full description' src="./Group 13.svg" alt="" />
+     <motion.img initial={{x:"200px",opacity:0}} whileInView={{x:0,opacity:1}} transition={{duration:0.7,ease:"easeInOut"}}  className='absolute bottom-[0%] w-full description' src="./Energised/el-2.png" alt="" />
       </div>
     </div>
     <div className='flex justify-center py-8'>
